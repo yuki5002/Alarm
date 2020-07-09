3447be984b45ee1b14f5402e5b588f2a9acc4e3e
yuki5002 <yuki.sunnies@gmail.com>
2020-07-09 20:08:24 +0900

    storybordでメインの画面を作って、アラームの機能を途中まで作った。

---
+++ Alarm/CurrentTime.swift
@@ -0,0 +1,33 @@
+//
+//  CurrentTimeViewController.swift
+//  Alarm
+//
+//  Created by 平石悠生 on 2020/07/09.
+//  Copyright © 2020 平石悠生. All rights reserved.
+//
+
+import UIKit
+
+class CurrentTime: UIViewController {
+    
+    var timer: Timer?
+    var cuurrentTime: 
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
